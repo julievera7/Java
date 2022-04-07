60597eae9af9dd7041b6976a1617ff7b7a8b7c29
aliet <julievera7@gmail.com>
2022-04-07 16:38:10 -0300

    Primera actividad del OpenBootCamp Java Inicial

---
+++ javaInicial/src/javainicial/JavaInicial.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javainicial;
+
+/**
+ *
+ * @author aliet
+ */
+public class JavaInicial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    		//Crear e inicializar variables
+		byte bnumero = 1;
+		short snumero = 12; 
+		int inumero = 50;
+		long lnumero = 123456;
+		float fnumero = (float) 2.2;
+		double dnumero = 22.22;
+		
+		char ctexto = 'c';
+		String stexto = "Cadena de texto";
+		
+		boolean verdadero = true;
+		boolean falso = false;
+
+		System.out.println("Tipo byte: " + bnumero);
+		System.out.println("Tipo short: " + snumero);
+		System.out.println("Tipo int: " + inumero);
+		System.out.println("Tipo long: " + lnumero);
+		System.out.println("Tipo float: " + fnumero);
+		System.out.println("Tipo double: " + dnumero);
+		System.out.println("Tipo char: " + ctexto);
+		System.out.println("Tipo String: " + stexto);
+		System.out.println("Tipo Boolean: " + verdadero);
+		System.out.println("Tipo Boolean: " + falso);
+	  
+    }
+    
+}
